28df60cf381714ac935d09d0306c70711cd1562c
kritak-dev <kritak.singh.che15@itbhu.ac.in>
2021-06-09 10:53:24 +0530

    Minor changes

--- src/contact.js
+++ src/contact.js
@@ -3,7 +3,7 @@ export function contact() {
     const contact = document.createElement('div');
     contact.id = 'contact';
     contact.textContent = 'This is the contacts page';
-    contact.classList.add('.text');
+    contact.classList.add('text');
 
     return { contact };
 }
--- src/home.js
+++ src/home.js
@@ -2,7 +2,7 @@ export function home() {
     const homePage = document.createElement('div');
     homePage.id = 'home';
     homePage.innerHTML = 'This is the home page';
-    homePage.classList.add('.text');
+    homePage.classList.add('text');
 
     return { homePage };
 }
