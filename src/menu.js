fa5ed6eef75254b74d3ece9eb9e402d0dea3b05b
kritak-dev <kritak.singh.che15@itbhu.ac.in>
2021-06-09 10:52:05 +0530

    Minor changes

--- src/menu.js
+++ src/menu.js
@@ -3,7 +3,7 @@ const menu = () => {
     const menu = document.createElement('div');
     menu.id = 'menu';
     menu.textContent = 'This is the menu page';
-    menu.classList.add('text');
+    menu.classList.add('.text');
     
     return { menu };
 }
